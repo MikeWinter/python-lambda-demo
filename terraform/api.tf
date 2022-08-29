eb54a5af7889eaa299c68f7b44206c8cc16c28f2
Mike Winter <mail@mikewinter.uk>
2022-08-29 22:44:35 +0100

    Add OpenAPI and API Gateway definition

--- terraform/api.tf
+++ terraform/api.tf
@@ -15,6 +15,21 @@ resource "aws_lambda_function" "toggles" {
   runtime = "python3.9"
 }
 
+resource "aws_lambda_function_url" "toggles" {
+  function_name      = aws_lambda_function.toggles.function_name
+  authorization_type = "NONE"
+}
+
+resource "aws_lambda_permission" "toggles" {
+  statement_id_prefix = "AllowAccessFromApiGateway-"
+
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.toggles.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/GET/*"
+}
+
 resource "aws_lambda_layer_version" "http-lib" {
   layer_name = "http-lib"
 
@@ -22,11 +37,6 @@ resource "aws_lambda_layer_version" "http-lib" {
   source_code_hash = filebase64sha256(local.http-lib-filename)
 
   compatible_runtimes = ["python3.9"]
-}
-
-resource "aws_lambda_function_url" "toggles" {
-  function_name      = aws_lambda_function.toggles.function_name
-  authorization_type = "NONE"
 }
 
 data "aws_iam_policy_document" "lambda_function_assume_role" {
@@ -47,4 +57,29 @@ resource "aws_iam_role" "toggles" {
 resource "aws_iam_role_policy_attachment" "toggles" {
   role       = aws_iam_role.toggles.name
   policy_arn = aws_iam_policy.read-toggles.arn
+}
+
+resource "aws_api_gateway_rest_api" "api" {
+  name = "API"
+  body = templatefile("../backend/openapi.yaml", {
+    toggles-arn = aws_lambda_function.toggles.invoke_arn,
+  })
+}
+
+resource "aws_api_gateway_deployment" "api" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+
+  triggers = {
+    api-description = sha256(aws_api_gateway_rest_api.api.body)
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "api" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  deployment_id = aws_api_gateway_deployment.api.id
+  stage_name    = "default"
 }
